a4c53d0bba526e901e920475f5f1618ced44c23a
aynaash <caynaashow@gmail.com>
2026-03-01 05:19:53 +0300

    chore: bump version to v0.1.33 for rollback implementation

--- shared/version.go
+++ shared/version.go
@@ -2,4 +2,4 @@ package shared
 
 // Version is the current CLI version. It is overridden at build time via
 // ldflags: -X github.com/Golangcodes/nextdeploy/shared.Version=<tag>
-const Version = "v0.1.32"
+const Version = "v0.1.33"
