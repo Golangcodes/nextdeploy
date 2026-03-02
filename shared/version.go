38603f8353a93cb572f553b62168281b1269aa59
aynaash <caynaashow@gmail.com>
2026-03-02 07:48:21 +0300

    chore: bump version to v0.1.34

--- shared/version.go
+++ shared/version.go
@@ -1,5 +1,3 @@
 package shared
 
-// Version is the current CLI version. It is overridden at build time via
-// ldflags: -X github.com/Golangcodes/nextdeploy/shared.Version=<tag>
-const Version = "v0.1.33"
+const Version = "v0.1.34"
